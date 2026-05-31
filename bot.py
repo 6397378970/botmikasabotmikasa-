7fbf92a0645c9b77cd546cdd1e9e70d2b6bfadfb
6397378970 <jay9900x@gmail.com>
2026-05-31 11:22:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ logger = logging.getLogger(__name__)
 
 TOKEN = "8674194296:AAGqxTPggfH52IyefdVP8565SFOJcmspOwI"
 
-OWNERS = ['Opx333', 'Rexxygodhu']
+OWNERS = ['Opx333', 'Rexxygodhu', 'light_speedy']
 
 GIFT_ITEMS = {
     'rose':       {'emoji': '🌹', 'price': 500,  'name': 'Rᴏꜱᴇ'},
